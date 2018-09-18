062f42444b471f46283a30b394ecf662093b70db
Markis145 <marc_mstr@hotmail.com>
2018-09-18 21:33:39 +0200

    1ra classe

--- hello.php
+++ hello.php
@@ -1,14 +1,28 @@
 <?php
 
-$greeting="hola moon";
+//$greeting="hola mon";
 
 //Xivatos Ctrl + / (teclat numeric)
 
 //echo "Hola mon"; // Hardcoded
 
-echo $greeting;
+//echo $greeting;
 //var_dump('hola mon');
 
 //-Xivatos: echo, dd, dump, Laravel dump server, logs, Laravel DebugBar, var_dump, <pre>
 
 // TEST -> TDD
+
+// funcions Predefinides -> venen de serie en php
+//var_dump();
+//
+//nom([Parametres]);
+
+// WHISFUL THINKING | WISHFUL PROGRAMMING
+
+function hello($who) {
+    //opcional //parametres opcionals
+    echo 'hola ' . $who;
+}
+
+hello('mon');
