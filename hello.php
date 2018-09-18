cb21f62bf1a06bf38f2a03840121c43fc8cc16bc
Markis145 <marc_mstr@hotmail.com>
2018-09-18 20:38:42 +0200

    xivatos PHP amb echo

---
+++ hello.php
@@ -0,0 +1,14 @@
+<?php
+
+$greeting="hola moon";
+
+//Xivatos Ctrl + / (teclat numeric)
+
+//echo "Hola mon"; // Hardcoded
+
+echo $greeting;
+//var_dump('hola mon');
+
+//-Xivatos: echo, dd, dump, Laravel dump server, logs, Laravel DebugBar, var_dump, <pre>
+
+// TEST -> TDD
