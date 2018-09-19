a2308194fc20cea49e1964375bada598211d2c4f
Markis145 <marc_mstr@hotmail.com>
2018-09-19 17:58:30 +0200

    pagines dinamiques

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ php -S localhost:8080 -> obrir servidor
 
 ##SEPARATION OF CONCERNS
 separar el que veu l'usuari (vistes) en el que veiem nosaltres (controladors, etc)
+
+HTTP -> Hypertext transfer protocol <- xarxes
+
+HTTP -> entre softwares
+
+CLIENT (BROWSER/navegador) (html+css+javascript) -> SERVIDOR (Apache/Nginx) (.php)
+
+PROTOCOL MISSATGES (2 MISSATGES)                                
+-HTTP REQUEST -> client->servidor               
+-HTTP RESPONSE -> servidor->client (PUSH)
+
+PROTOCOL MISSATGES (TIPUS/METHODS)
+- GET -> per defecte
+- POST
+- DELETE
+- PUST
--- index.php
+++ index.php
@@ -2,6 +2,38 @@
 
 require 'functions.php';
 
+//var_dump($_GET);
+//die();
+
+//String -> $greeting='Hola';
+//Booleans -> $x = true/false;
+//Enter -> $num = 15;
+
+//Array/Vector
+
+//$fruites = array();
+//$fruites = [];
+
+//Normal array
+//$str = 'platan';
+//$str1 = 'presec';
+//$str2 = 'taroja';
+//
+//$fruites = [$str, $str1, $str2];
+//
+//echo $fruites[0];
+//die();
+
+//Array associatiu
+
+//$person="Marc Mestre Alguero";
+
+//$person = [
+//    'name' => 'Marc Mestre Alguero',
+//    'dni' => '123123123K',
+//    'mobile' => '12358125'
+//];
+
 $greeting=hello('mon');
 
 require 'views/index.blade.php';
